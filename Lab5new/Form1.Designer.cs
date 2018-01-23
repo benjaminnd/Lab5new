40ad6858cc65c037e6aacc33f52a0f34ff2faeca
benjaminnd <minhnhatdang1111@gmail.com>
2018-01-23 10:51:57 -0500

    Add project files.

---
+++ Lab5new/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Lab5new
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Colors = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Colors
+            // 
+            this.Colors.Location = new System.Drawing.Point(53, 62);
+            this.Colors.Name = "Colors";
+            this.Colors.Size = new System.Drawing.Size(86, 35);
+            this.Colors.TabIndex = 0;
+            this.Colors.Text = "Colors";
+            this.Colors.UseVisualStyleBackColor = true;
+            this.Colors.Click += new System.EventHandler(this.Colors_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(175, 62);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 35);
+            this.Close.TabIndex = 1;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(53, 244);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(123, 84);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(53, 155);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(146, 56);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(436, 650);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Colors);
+            this.Name = "Form1";
+            this.Text = "Water drop app";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Colors;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Lab5new/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5new
+    /**
+    *I, Nhat Minh Dang, 000746305 certify that this material is my original work. No other person's work has been used without due acknowledgement.
+    */
+{
+    public partial class Form1 : Form
+    {
+        private ColorDialog ColorBox = new ColorDialog(); // create Color box
+        private Graphics g; // create Graphic object
+        private Color c = Color.Black; 
+        private Pen p; //create a pen
+        private SolidBrush b; 
+        private int defaultInterval = 150; //set default Interval of Timer to 150 miliseconds
+        private int count = 1; //ticks counter
+        private int previousCount;
+        private Timer timer = new Timer();
+        bool filled;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            timer.Tick += new EventHandler(timer_Tick); // Everytime timer ticks, timer_Tick will be called
+            timer.Interval = defaultInterval;            // Timer will tick every
+            b = new SolidBrush(c); //Intansitate black brush
+            filled = false;
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics(); //create Graphics object
+            g.DrawLine(p, 101, 450 - count, 299, 450 - count); //Each time timer tick draw a new line inside the bucket representing the liquid filling
+            count++; //increase ticks counter
+            if (count == 150) //if bucket is filled
+            {
+                filled = true; //set bool to true
+                //turn off timer
+                timer.Enabled = false; 
+                timer.Stop();
+                previousCount = count; //store count to previousCount
+                count = 1; //set tick counter back to 1
+                timer.Interval = defaultInterval; //set interval back to default
+                trackBar1.Value = 0; //set trackbar to default
+                g.DrawLine(new Pen(Color.Black, 20), 120, 270, 120, 450 - previousCount + 1); //turn off the tap
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            p = new Pen(c);
+            Pen tempPen = new Pen(Color.White);
+            //draw lines representing bucket borders
+            g.DrawLine(tempPen, 100, 300, 100, 450);
+            g.DrawLine(tempPen, 100, 450, 300, 450);
+            g.DrawLine(tempPen, 300, 300, 300, 450);
+
+        }
+        private void Colors_Click(object sender, EventArgs e)
+        {
+            ColorBox.Color = c; 
+            ColorBox.ShowDialog(); //show Colordialog
+            c = ColorBox.Color;  
+            p = new Pen(c);
+            if (timer.Enabled) g.DrawLine(new Pen(ColorBox.Color, 20), 120, 270, 120, 450 - count); //draw new filling liquid if the color is selected and the timer is running
+
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int trackBarVal = trackBar1.Value;
+            g = this.CreateGraphics();
+            //if trackbar val  is 0 stop timer and stop the faucet
+            if (trackBarVal == 0)
+            {
+                timer.Stop();
+                g.DrawLine(new Pen(Color.Black, 20), 120, 270, 120, 450 - count);
+            }
+            else
+            {
+                //if the bucket is filled , empty the bucket 
+                if (filled)
+                {
+                    g.FillRectangle(b, 101, 299, 198, 149); //empty the bucket
+                    filled = false; 
+                }
+                //turn on the faucet and modify interval of the timer
+                g.DrawLine(new Pen(ColorBox.Color, 20), 120, 270, 120, 450 - count);
+                if (!timer.Enabled) timer.Start(); 
+                if(trackBarVal >= 1 && trackBarVal <= 3)
+                {
+                    timer.Interval = defaultInterval - 50;
+                }else if(trackBarVal >= 4 && trackBarVal <= 7)
+                {
+                    timer.Interval = defaultInterval - 100;
+                }
+                else
+                {
+                    timer.Interval = defaultInterval - 145;
+                }
+            }
+
+
+        }
+
+
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+    }
+}
